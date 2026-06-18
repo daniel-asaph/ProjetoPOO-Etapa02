0f3bf476b328773dbdaf30796ef08d53b1d5fce3
Your NameDaniel <danielasaph12@gmail.com>
2026-06-18 15:09:43 -0300

    Criando classe Pessoa e refatorando Paciente

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,49 +1,37 @@
-public class Paciente {
-    public String nome;
-    public String cpf;
-    public int idade;
-    public String telefone;
-    public String convenioNome;
-    public boolean ativo;
+public class Paciente extends Pessoa{
+    private int idade;
+    private String convenioNome;
+    private boolean ativo;
 
     public Paciente(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.idade = 0;
-        this.telefone = "";
+        super(nome, cpf);
         this.convenioNome = "";
         this.ativo = true;
     }
 
-    public Paciente(String nome, String cpf, int idade, String telefone) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.idade = idade;
-        this.telefone = telefone;
+    public Paciente(String nome, String cpf, String telefone, int idade) {
+        super(nome, cpf, telefone, idade);
         this.convenioNome = "";
         this.ativo = true;
     }
 
     // construtor com todos os dados
-    public Paciente(String nome, String cpf, int idade, String telefone, String convenioNome) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.idade = idade;
-        this.telefone = telefone;
+    public Paciente(String nome, String cpf, String telefone, int idade, String convenioNome) {
+        super(nome, cpf, telefone, idade);
         this.convenioNome = convenioNome;
         this.ativo = true;
     }
 
     // atualiza so idade e telefone
     public void complementar(int idade, String telefone) {
-        this.idade = idade;
-        this.telefone = telefone;
+        setIdade(idade);
+        setTelefone(telefone);
     }
 
     // atualiza tudo incluindo convenio
     public void complementar(int idade, String telefone, String convenioNome) {
-        this.idade = idade;
-        this.telefone = telefone;
+        setIdade(idade);
+        setTelefone(telefone);
         this.convenioNome = convenioNome;
     }
 
@@ -51,6 +39,7 @@ public class Paciente {
         this.ativo = false;
     }
 
+    @Override
     public String exibirResumo() {
         String status = "Sim";
         if (!ativo) {
---
+++ src/Pessoa.java
@@ -0,0 +1,59 @@
+public abstract class Pessoa {
+    protected String nome;
+    protected String cpf;
+    protected String telefone;
+    protected int idade;
+
+    public Pessoa(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    public Pessoa(String nome, String cpf, String telefone, int idade){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.idade = idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        if(nome == null || nome.isEmpty()){
+            System.out.println("Nome não pode ser vazio");
+            return;
+        }
+        this.nome = nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        if(cpf == null || cpf.isEmpty()){
+            System.out.println("Cpf não pode ser vazio");
+            return;
+        }
+        this.cpf = cpf;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        if(telefone == null || telefone.isEmpty()){
+            System.out.println("Numero de telefone não pode ser vazio");
+            return;
+        }
+        this.telefone = telefone;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public abstract String exibirResumo();
+}
