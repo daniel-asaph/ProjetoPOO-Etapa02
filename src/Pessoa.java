3652dcf09f10b056db47fc73c1a8d96ce9eb1f2d
Ana Julia <seu-email-anajuliafernandes530@gmail.com>
2026-06-24 13:46:13 -0300

    Salvando a criacao da classe Pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,29 @@
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String dataNascimento;
+
+    public Pessoa(String nome, String cpf, String telefone, String dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+}
